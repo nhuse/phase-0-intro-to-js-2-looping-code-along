2569606485d549b3bd159b0776202c79d8455580
Nathan <nathan.huse@yahoo.com>
2021-05-12 11:09:40 -0500

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+function writeCards(arrayOfNames, eventName) {
+    const newArr = [];
+    for(let count=0; count<arrayOfNames.length; count++) {
+        newArr[count]=`Thank you, ${arrayOfNames[count]}, for the wonderful ${eventName} gift!`;
+    }
+    return newArr;
+}
